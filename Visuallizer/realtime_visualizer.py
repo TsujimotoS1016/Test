76c3cc2d55ae7811fba33e28c3652409bb935479
TSUJIMOTO SHONOSUKE <tsujimotos1016@gmail.com>
2025-10-19 19:07:21 +0900

    little fix

--- Visuallizer/realtime_visualizer.py
+++ Visuallizer/realtime_visualizer.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
 import sounddevice as sd
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -17,6 +18,8 @@ volume = 0
 with sd.InputStream(channels=1, callback=audio_callback,
                     blocksize=blocksize, samplerate=samplerate):
     running = True
+    x = 400 #中心
+    y = 300
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,7 +27,14 @@ with sd.InputStream(channels=1, callback=audio_callback,
 
         screen.fill((0, 0, 0))
         radius = int(volume)  # 音量に応じて半径変化
-        pygame.draw.circle(screen, (100, 200, 255), (400, 300), radius)
+        pygame.draw.circle(screen, (100, 200, 255), (x, y), radius)
+        x += random.randint(-15, 15)
+        y += random.randint(-15, 15)
+
+        # 画面外に出ないように制限
+        x = max(0, min(800, x))
+        y = max(0, min(600, y))
+
         pygame.display.flip()
         clock.tick(60)
 
